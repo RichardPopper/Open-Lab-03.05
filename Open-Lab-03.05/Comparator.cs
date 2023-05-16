7485f5097f600e72c80d5ebd86a0c2ea0abab194
RichardPopper <richard.popper11@gmail.com>
2023-05-16 21:19:41 +0200

    add method that

--- Open-Lab-03.05/Comparator.cs
+++ Open-Lab-03.05/Comparator.cs
@@ -6,15 +6,7 @@ namespace Open_Lab_03._05
     {
         public bool MatchCaseInsensitive(string str1, string str2)
         {
-            bool equuals = string.Equals(str1,str2, StringComparison.OrdinalIgnoreCase);
-             if (equuals==true)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return string.Equals(str1, str2, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
